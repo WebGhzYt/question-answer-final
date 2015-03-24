8ad2ab84e2787e7a8e3ccd161f0126cee1f0a58b
rhtprwl <rohit.porwal@ongraph.com>
2015-03-24 14:40:49 +0530

    Accourding to probleme-1

--- app/mailers/questionmailer.rb
+++ app/mailers/questionmailer.rb
@@ -1,8 +1,10 @@
 class Questionmailer < ApplicationMailer
 	default from: "engrohitjain5@gmail.com"
 	def welcome_email(question)
+		# @question.ans_mail_id
     	@question = question
+    	# @question_mail_id[] = @question.ans_mail_id.split(",")
 	    @url  = 'localhost:3000'
-    	mail(to: @question.ans_mail_id, subject: 'Question from localhost')
+    	mail(to: @question.ans_mail_id.split(","), subject: 'Question from localhost')
   	end
 end
